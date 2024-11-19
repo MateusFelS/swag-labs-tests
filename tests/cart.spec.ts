5ad91dade92091a49a1cf7a597da858a992f0508
MateusFels <mateus.fellype14@gmail.com>
2024-11-19 12:27:29 -0300

    colocando checkout em um arquivo separado

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,7 +1,5 @@
 import { expect, test, Page } from "@playwright/test";
 
-const successCheckoutMessage = "Thank you for your order!";
-const errorCheckoutMessage = "Error: First Name is required";
 const baseUrl = "https://www.saucedemo.com/";
 
 // Função para adicionar um produto ao carrinho
@@ -12,19 +10,6 @@ async function addProductToCart(page: Page, productId = "sauce-labs-backpack") {
 // Função para navegar para o carrinho
 async function goToCart(page: Page) {
   await page.locator('[data-test="shopping-cart-link"]').click();
-}
-
-// Função para iniciar o checkout
-async function startCheckout(page: Page) {
-  await page.locator('[id="checkout"]').click();
-}
-
-// Função para preencher o formulário de informações
-async function fillCheckoutForm(page: Page, firstName: string, lastName: string, postalCode: string) {
-  await page.locator('[id="first-name"]').fill(firstName);
-  await page.locator('[id="last-name"]').fill(lastName);
-  await page.locator('[id="postal-code"]').fill(postalCode);
-  await page.locator('[id="continue"]').click();
 }
 
 // Função de login antes de cada teste
@@ -64,53 +49,5 @@ test("Remover produto do carrinho", async ({ page }) => {
 
   await test.step("Verifique se tem algum item no carrinho", async () => {
     expect(page.locator('[data-test="inventory-item"]')).toBeHidden();
-  });
-});
-
-test("Realizar um checkout com sucesso", async ({ page }) => {
-  await test.step("Adicione um item ao carrinho", async () => {
-    await addProductToCart(page);
-  });
-
-  await test.step("Vá para a página do carrinho", async () => {
-    await goToCart(page);
-  });
-
-  await test.step("Clique em checkout", async () => {
-    await startCheckout(page);
-  });
-
-  await test.step("Complete o formulário Your Information e clique no botão Continue", async () => {
-    await fillCheckoutForm(page, "Fulano", "Ciclano", "18870000");
-  });
-
-  await test.step("Clique no botão Finish", async () => {
-    await page.locator('[id="finish"]').click();
-  });
-
-  await test.step("Uma mensagem 'Thank you for your order!' deve aparecer", async () => {
-    expect(page.locator('[data-test="complete-header"]')).toHaveText(successCheckoutMessage);
-  });
-});
-
-test("Realizar um checkout com formulário em branco", async ({ page }) => {
-  await test.step("Adicione um item ao carrinho", async () => {
-    await addProductToCart(page);
-  });
-
-  await test.step("Vá para a página do carrinho", async () => {
-    await goToCart(page);
-  });
-
-  await test.step("Clique em checkout", async () => {
-    await startCheckout(page);
-  });
-
-  await test.step("Deixe em branco o formulário Your Information e clique no botão Continue", async () => {
-    await fillCheckoutForm(page, "", "", "");
-  });
-
-  await test.step("Uma mensagem 'Error: First Name is required' deve aparecer", async () => {
-    expect(page.locator('[data-test="error"]')).toHaveText(errorCheckoutMessage);
   });
 });
